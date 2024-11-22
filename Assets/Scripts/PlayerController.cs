78691f8d7487072149dd287f40f9e374441be7ac
C0SMICSHARK <Xanderrowley42@gmail.com>
2024-11-22 15:57:09 +0000

    Completed Module.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,10 @@ public class PlayerController : MonoBehaviour
 {
    // Floats, variables etc.
     
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     
     
     
@@ -23,9 +24,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Move the Vehicle forward.
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        // Allow the car to move side to side.
-        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // Move the Vehicle forward based on vertical input.
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // rotates the car based on horizontal input.
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
     }
 }
